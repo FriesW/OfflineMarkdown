7e4baaf6ac8b72b9aed59bc6fb525a4084deacba
FriesW <william.james.fries@gmail.com>
2018-02-16 15:04:58 -0600

    Buttons & Bugs
    Buttons have now been hooked up to some degree.
    I'm pretty sure there is a bug in BehaveHooks, such that when the editor
    is empty, a tab will be inserted on construction. Instead of fixing it,
    I have worked around it.

--- package/support/main.js
+++ package/support/main.js
@@ -18,6 +18,15 @@ window.onload = function()
     if ( !(window.File && window.FileReader && window.FileList && window.Blob && typeof(Storage) !== "undefined") )
         alert('Oh no! It looks like you have an outdated web browser. This web page is probably broken...\nProceed with caution.');
     
+    //Setup buttons
+    gid('btnClear').addEventListener('click', function(){
+        gid('input').value = '';
+        update();
+    });
+    gid('btnSave').addEventListener('click', function(){
+        alert('update');
+    });
+    
     //Setup the showdown markdown processor
     converter = new showdown.Converter();
     converter.setFlavor('github');
@@ -28,15 +37,15 @@ window.onload = function()
     });
     
     //Setup input change
-    gid('input').addEventListener('input', update);
-    BehaveHooks.add('keydown', update);
+    BehaveHooks.add('keyup', update);
     
     //Check for previous session
     if( localStorage.last )
-    {
         gid('input').value = localStorage.last;
-        update();
-    }
+    else
+        gid('input').value = '';
+    
+    update();
     
 
 
