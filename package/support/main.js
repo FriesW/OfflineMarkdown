d8ea8fbd35ea49071f9e0c25e428d63fe54f407f
FriesW <william.james.fries@gmail.com>
2018-02-16 12:04:28 -0600

    Add Library Behave
    Makes text area act more like a proper editor.

--- package/support/main.js
+++ package/support/main.js
@@ -3,18 +3,30 @@ function gid(id)
     return document.getElementById(id);
 }
 
+function update()
+{
+    gid('output').innerHTML = converter.makeHtml(gid('input').value);
+}
+
 
 var converter;
 
 window.onload = function()
 {
-    
+    //Setup the showdown markdown processor
     converter = new showdown.Converter();
     converter.setFlavor('github');
     
-    gid('input').addEventListener('input', function(){
-        gid('output').innerHTML = converter.makeHtml(gid('input').value);
+    //Setup editor
+    var editor = new Behave({
+        textarea: gid('input')
     });
+    
+    //Setup input change
+    gid('input').addEventListener('input', update);
+    BehaveHooks.add('keydown', update);
+    
+    
     
     
 
