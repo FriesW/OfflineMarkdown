af4a84b3c4ff7a1f5b943e10c9967b270f659082
FriesW <william.james.fries@gmail.com>
2018-02-21 18:29:38 -0600

    Util: Var type test methods

--- package/support/util.js
+++ package/support/util.js
@@ -6,3 +6,20 @@ function gid(id)
 {
     return document.getElementById(id);
 }
+
+function is_type(item, type)
+{
+    if( typeof type !== 'string')
+        throw "is_type: type parameter must be a string.";
+    return typeof item === type;
+}
+
+function is_string(item)
+{
+    return is_type(item, 'string');
+}
+
+function is_set(item)
+{
+    return is_type(item, 'undefined');
+}
